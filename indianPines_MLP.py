75e5ca7931adf8163faaa048b203fcdc5b8fc629
git4dev2000 <git4dev2000@outlook.com>
2018-10-19 11:57:48 -0500

    prepro

--- indianPines_MLP.py
+++ indianPines_MLP.py
@@ -9,87 +9,86 @@ import numpy as np
 from scipy import io as sio
 from keras import models, layers, optimizers, metrics, losses, regularizers
 import os
-#from matplotlib import pyplot as plt
 import math
 import preprocessing
 from matplotlib import pyplot as plt
-# Loading dataset
+
 
 data_folder = '/home/mansour/imgANN/Datasets'
 data_file= 'Indian_pines_corrected'
 gt_file = 'Indian_pines_gt'
-train_fraction = 0.85
+train_fraction = 0.85 # with respect to the entire data. 
+val_fraction = 0.05 #with rescept to training set.
 rem_classes = [0]
+patch_size = 1
+lr = 1e-4
+units_1 = 2**8
+drop_rate =0.35
+batch_size = 2**3
+#units_2 = 2**8
 class_weights = dict([(1,33),(2,200),(3,200),(4,181),(5,200),(6,200),(7,20),
                       (8,200),(9,14),(10,200),(11,200),(12,200),(13,143),(14,200),
                       (15,200),(16,75)])
-patch_size = 1
-lr = 1e-4
-#units_1 = 200
-units_2 = 2**8
 
 
-# The corrected Salinas dataset has a coverage of 512*217(height*width) and 204 channels
-# It represents 16 different classes. Each chnnel contains different ranges of values.
- 
+# Loading dataset 
 data_set = sio.loadmat(os.path.join(data_folder, data_file)).get('indian_pines_corrected')
 gt = sio.loadmat(os.path.join(data_folder, gt_file)).get('indian_pines_gt')
 
-# Preprocessing for data split for trainingand test
+# Preprocessing for data split for training and test
 (train_rows, train_cols), (test_rows, test_cols) = preprocessing.data_split(gt, 
-train_fraction=train_fraction, rem_classes=rem_classes, split_method=class_weights) # may set to 'same_hist'
+train_fraction=train_fraction, rem_classes=rem_classes, split_method='same_hist') # may set to 'same_hist'
 
 # Reducing dataset dimension using PCA
 #data_set = preprocessing.reduce_dim(data_set, .999)
 
 # Scaling the input data using mean and std at each band
-data_set_scaled = np.zeros(data_set.shape)
-for i in np.arange(data_set.shape[2]):
-    band = data_set[:,:,i]
-    data_set_scaled[:,:,i] = (band - np.mean(band)) / np.std(band) # zero mean and 1 std
-    #data_set_scaled[:,:,i] = band / np.amax(band)  # scaled with max
-data_set = data_set_scaled
-    
-# Creating input tensor and class labels
-train_pixel_indices = (train_rows, train_cols)
-train_input, train_labels = preprocessing.create_patch(data_set=data_set,
-                                    gt=gt,
-                                    pixel_indices=train_pixel_indices,
-                                    patch_size=patch_size)
+data_set = preprocessing.rescale_data(data_set)
 
+# Preparing input tensors for training, validation and test sets:
+(train_rows_sub, train_cols_sub), (val_rows, val_cols) = preprocessing.val_split(
+        train_rows, train_cols, gt, val_fraction=val_fraction)
+
+train_pixel_indices_sub = (train_rows_sub, train_cols_sub)
+val_pixel_indices = (val_rows, val_cols)
 test_pixel_indices = (test_rows, test_cols) 
-test_input, test_labels = preprocessing.create_patch(data_set=data_set,
-                                    gt=gt,
-                                    pixel_indices=test_pixel_indices,
-                                    patch_size=patch_size)
+catg_labels = np.unique([int(gt[idx[0],idx[1]]) for idx in zip(train_rows, train_cols)])
+num_catg = len(catg_labels)
+int_to_vector_dict = preprocessing.label_2_one_hot(catg_labels)
 
-
-
-# Calculating number of categories
-num_catg = len(np.unique(train_labels))
-
-# Creating label to one-hot dictionary...
-int_to_vector_dict = preprocessing.lebel_2_one_hot(train_labels)
-
-# Creating label tensor to be used for a nn_model
-y_train = np.array([int_to_vector_dict.get(elm) for elm in train_labels])
-y_test = np.array([int_to_vector_dict.get(elm) for elm in test_labels])
-
-
+train_input_sub, y_train_sub = preprocessing.create_patch(
+        data_set=data_set,
+        gt=gt,
+        pixel_indices=train_pixel_indices_sub,
+        patch_size=patch_size,
+        label_vect_dict=int_to_vector_dict)
+val_input, y_val = preprocessing.create_patch(
+        data_set=data_set,
+        gt=gt,
+        pixel_indices=val_pixel_indices,
+        patch_size=patch_size,
+        label_vect_dict=int_to_vector_dict)
+test_input, y_test = preprocessing.create_patch(
+        data_set=data_set,
+        gt=gt,
+        pixel_indices=test_pixel_indices,
+        patch_size=patch_size,
+        label_vect_dict=int_to_vector_dict)
 
 # Building a MLP network model
+input_shape = (patch_size, patch_size, data_set.shape[-1])
 nn_model = models.Sequential()
 #
-# dense_1
+# dense_input
 nn_model.add(layer=layers.Dense(units=data_set.shape[2], activation='relu',
-                                input_shape=train_input.shape[1:]
-                                ))
-# flatten to chnage input shape from (1,1,num_band) to (num_band,)
+                                input_shape=input_shape))
+# flatten_1, changes input shape from (1,1,num_band) to (num_band,)
 nn_model.add(layer=layers.Flatten())
-nn_model.add(layer=layers.Dense(units=int(2/3*(num_catg+train_input.shape[3])), activation='relu'))
-#nn_model.add(layer=layers.Dense(units=2**10, activation='relu'))
-#nn_model.add(layer=layers.Dense(units=2**10, activation='relu'))
-#nn_model.add(layer=layers.Dropout(0.35))
+# dense_1
+nn_model.add(layer=layers.Dense(units=units_1, activation='relu')) # could be set to units=int(2/3*(num_catg+input_shape[-1]))
+# dropout_1
+nn_model.add(layer=layers.Dropout(drop_rate))
+# dense_output
 nn_model.add(layer=layers.Dense(units=num_catg, activation='softmax'))
 
 
@@ -98,10 +97,10 @@ nn_model.compile(optimizer=optimizers.RMSprop(lr=lr),
                  loss=losses.categorical_crossentropy,
                  metrics=[metrics.categorical_accuracy])
 
-history = nn_model.fit(x=train_input, y=y_train, batch_size=2**3, epochs=50,
-                       validation_split=0.05)
+# Training the model
+history = nn_model.fit(x=train_input_sub, y=y_train_sub, batch_size=batch_size,
+                       epochs=50, validation_data=(val_input, y_val))
     
-
 
 # Plotting history
 epoches = np.arange(1,len(history.history.get('loss'))+1)
@@ -113,43 +112,23 @@ plt.plot(epoches, history.history.get('categorical_accuracy'), 'b',label='Accura
 plt.plot(epoches, history.history.get('val_categorical_accuracy'),'bo', label='Validation_Accu')
 plt.legend()
 
-# Calculating metrics on test data
+# Calculating performance metrics for test data
+model_metrics = preprocessing.calc_metrics(nn_model, test_input,
+                                           y_test, int_to_vector_dict)
+
+#nn_model.save('indian_pines_MLP2_2.h5')
 #
-# Creating one_hot_2_int dictionary...
-vectot_2_label = preprocessing.one_hot_2_label(int_to_vector_dict)
-test_catgs, test_catg_counts = np.unique([vectot_2_label.get(tuple(elm)) for elm
-                                          in y_test], return_counts=True)
+# Plotting predicted results
+concat_rows =  np.concatenate((train_rows_sub, val_rows, test_rows))
+concat_cols = np.concatenate((train_cols_sub, val_cols, test_cols))
+concat_input = np.concatenate((train_input_sub, val_input, test_input))
+concat_y = np.concatenate((y_train_sub, y_val, y_test))
+pixel_indices = (concat_rows, concat_cols)
 
-# Generating a list of tuples for storing pixel coordinate, i.e
-# with format (catg_lable, row, col, input_tensor, target_tensor, metric_container)
-from_to_list = []
-num_metrics = len(nn_model.metrics_names)
-res_container = [(elm,[],[],[],[],[]) for elm in test_catgs]
+partial_map = preprocessing.plot_partial_map(nn_model, gt, pixel_indices, concat_input,
+                            concat_y, int_to_vector_dict)
 
-i=0
-for elm in test_catg_counts:
-    from_idx = i 
-    to_idx = i + elm
-    i+=elm
-    from_to_list.append((from_idx, to_idx))
-
-
-for elm in zip(res_container, from_to_list): 
-    elm[0][1].append(test_rows[elm[1][0]:elm[1][1]]) # catg row
-    elm[0][2].append(test_cols[elm[1][0]:elm[1][1]]) # catg col
-    x=test_input[elm[1][0]:elm[1][1], :, :, :]
-    y=y_test[elm[1][0]:elm[1][1],:]
-    #elm[0][3].append(test_input[elm[1][0]:elm[1][1], :, :, :]) # input_tensor
-    #elm[0][4].append(y_test[elm[1][0]:elm[1][1],:]) # predicted tensor
-    test_metrics= nn_model.evaluate(x=x, y=y) 
-    elm[0][5].append(test_metrics) #metric
-
-for elm in res_container:
-    print((elm[0],elm[-1]))
-
-
-
-
+full_map = preprocessing.plot_full_map(nn_model, data_set, gt, int_to_vector_dict, patch_size)
 
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -8,23 +8,18 @@ Created on Thu Sep 27 10:30:56 2018
 import numpy as np
 import math
 from sklearn.decomposition import PCA
-
-#from scipy import io as sio
-#from keras import models, layers, optimizers, metrics, losses
-#import os
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 
-# Preprocessing for data split for training, val, and test
 def data_split(gt, train_fraction=0.7, rem_classes=None,
                split_method='same_hist'):
     """
-    Outpus list of row and column indices for training and test sets.
+    Outputs list of row and column indices for training and test sets.
     
     Arguments
     ---------
     gt : numpy array
-        A 2-D Numpy array, containing integer values representing class ids.
+        A 2-D numpy array, containing integer values representing class ids.
         
     train_fraction : float 
         The ratio of training size to the entire dataset.
@@ -40,7 +35,7 @@ def data_split(gt, train_fraction=0.7, rem_classes=None,
     Returns
     -------
     out : 2-D tuple
-        Containts lists of rows and column indices for training
+        Contains lists of rows and column indices for training
         and test sets: (train_rows, train_cols), (test_rows, test_cols)
     """
 
@@ -118,7 +113,7 @@ def reduce_dim(img_data, n_components=0.95):
         
     Returns
     -------
-    out : 3-D numpy.ndarray
+    img_data_transformed : 3-D numpy.ndarray
         Contains transformed data with shape (height, width, n_components).
         
 
@@ -158,7 +153,7 @@ def create_patch(data_set, gt, pixel_indices, patch_size=5, label_vect_dict=None
     gt : A 2-D numpy.ndarray
         Contains integers, representing different categories.
         
-    pixel_indices : A seuence of two sequences.
+    pixel_indices : A sequence of two sequences.
         Contains lists of integers, representing training pixel rows and columns.
         e.g., (train_rows, train_cols), where train_rows and train_cols are list
         of integers.
@@ -220,7 +215,8 @@ def label_2_one_hot(label_list):
     
     Arguments
     ---------
-    label_list : A list of integers representing class labels.
+    label_list : list of integers
+        Contains class labels.
     
     Returns
     -------
@@ -277,7 +273,7 @@ def rescale_data(data_set, method='standard'):
     Arguments
     ---------
     data_set : 3-D numpy.ndarray
-        Containts image data with format: (height, width, channels).
+        Contains image data with format: (height, width, channels).
         
     methodod : str 
         Represents rescaling method. Can take one of: 'standard', 'zero_mean',
@@ -312,13 +308,14 @@ def rescale_data(data_set, method='standard'):
     
     return rescale_data
 
-def calc_metrics(nn_model, test_inputs, y_test, int_to_vector_dict):
+def calc_metrics(nn_model, test_inputs, y_test, int_to_vector_dict, verbose=True):
     """
     Calculates model performance metrics on test data.
     
     Arguments
     ---------
-    nn_model : Trained neural network model with metrics information.
+    nn_model : keras model.
+        Trained neural network model containing metrics information.
     
     test_inputs : numpy.ndarray
         Input tensor containing test inputs.
@@ -335,17 +332,14 @@ def calc_metrics(nn_model, test_inputs, y_test, int_to_vector_dict):
         A dictionary with int keys representing category labels and list of
         model error and performance metrics as values. 
     """
-    vectot_2_label = one_hot_2_label(int_to_vector_dict)
-    test_catgs, test_catg_counts = np.unique([vectot_2_label.get(tuple(elm)) for elm
+    vector_2_label = one_hot_2_label(int_to_vector_dict)
+    test_catgs, test_catg_counts = np.unique([vector_2_label.get(tuple(elm)) for elm
                                               in y_test], return_counts=True)
     
     # Generating a list of tuples for storing pixel coordinate, i.e
-    # with format (catg_lable, row, col, input_tensor, target_tensor, metric_container)
+    # with format (catg_label, metric_container)
     from_to_list = []
-    #test_rows = pixel_indices[0]
-    #test_cols = pixel_indices[1]
-    #num_metrics = len(nn_model.metrics_names)
-    res_container = [(elm,[],[],[],[],[]) for elm in test_catgs]
+    res_container = [(elm, []) for elm in test_catgs]
     
     i=0
     for elm in test_catg_counts:
@@ -356,25 +350,121 @@ def calc_metrics(nn_model, test_inputs, y_test, int_to_vector_dict):
     
     
     for elm in zip(res_container, from_to_list): 
-        #elm[0][1].append(test_rows[elm[1][0]:elm[1][1]]) # catg row
-        #elm[0][2].append(test_cols[elm[1][0]:elm[1][1]]) # catg col
         x=test_inputs[elm[1][0]:elm[1][1], :, :, :]
         y=y_test[elm[1][0]:elm[1][1],:]
-        #elm[0][3].append(test_input[elm[1][0]:elm[1][1], :, :, :]) # input_tensor
-        #elm[0][4].append(y_test[elm[1][0]:elm[1][1],:]) # predicted tensor
         test_metrics= nn_model.evaluate(x=x, y=y) 
         elm[0][-1].append(test_metrics) #metric
     
     model_metrics = dict([(elm[0], elm[-1]) for elm in res_container])
+    if verbose:
+        for key, val in model_metrics.items():
+            print(key, val)
     return model_metrics
+
+def plot_partial_map(nn_model, gt, pixel_indices, input_tensor, targ_tensor,
+             int_to_vector_dict, plo=True):
+    """
+    Plots prediction map using a trained model and inputs.
     
-   
+    Arguments
+    ---------
+    nn_model : A trained keras neural network model.
+        Trained using input data. 
+    
+    gt : numpy.ndarray
+        A 2-D numpy array containing int labels.
+        
+    pixel_indices : tuple of arrays
+        A tuple of length two containing arrays of rows and columns of input
+        pixels with format: (row_array, col_array)
+        
+    input_tensor : numpy.ndarray
+        Contains input_tensor consistent with the nn_model inputs.
+        
+    targ_tensor : numpy.ndarray
+        Target tensor, containing one_hot format of label data.
+        
+    int_to_vector_dict : dictionary
+        Associates int labels to their corresponding one_hot format. Can be
+        created using label_2_one_hot function.
+        
+    plo : logical
+        If True, plots the map.
+        
+    Returns
+    -------
+    gt_pred_map : numpy.ndarray
+        A 2-D numpy.ndarray, representing predicted labels.
+    
+    """
+    rows, cols = pixel_indices[0], pixel_indices[1]
+    vect_2_label_dict = one_hot_2_label(int_to_vector_dict)
+    y_pred_vectors = nn_model.predict(input_tensor, batch_size=1)   
+    y_pred = np.zeros(y_pred_vectors.shape[0], dtype=int)
+    for elm in enumerate(y_pred_vectors):
+        max_idx, *not_used = np.where(elm[1]==np.amax(elm[1]))
+        predicted_vec = np.eye(1,y_pred_vectors.shape[1],k=max_idx[0], dtype=int).ravel()
+        y_pred[elm[0]] = vect_2_label_dict.get(tuple(predicted_vec))
+    
+    map_shape=gt.shape
+    gt_pred_map = np.zeros(map_shape, dtype=int)
+    for elm in enumerate(zip(rows, cols)):
+        gt_pred_map[elm[1]] = y_pred[elm[0]] 
+    
+    if plo:
+        plt.imshow(gt_pred_map)
+    return gt_pred_map
+
+def plot_full_map(nn_model, data_set, gt, int_to_vector_dict, patch_size, plo=True):
+    """
+    Plots prediction map for the entire pixels.
+    
+    Arguments
+    ---------
+    nn_model : keras model.
+        Trained using input data. 
+    data_set : numpy.ndarray
+        Contains image data with 'channel_last' format, i.e., (height, width, channels)
+        
+    int_to_vector_dict : dictionary
+        Associates int labels to their corresponding one_hot format. Can be
+        created using label_2_one_hot function.
+        
+    patch_size : int
+        Represents patch size used for nn_model.
+        
+    plo : logical
+        If True, plots the map.
+        
+    Returns
+    -------
+    gt_pred_all_map : numpy.ndarray
+        A 2-D numpy.ndarray, representing predicted labels for all pixels.
+    """
+    rr, cc = np.meshgrid(np.arange(gt.shape[0]), np.arange(gt.shape[1]))
+    all_pixel_indices = (rr.ravel(), cc.ravel())
+    vector_2_label = one_hot_2_label(int_to_vector_dict)
+    all_inputs, all_labels = create_patch(data_set, gt, all_pixel_indices,
+                                          patch_size, int_to_vector_dict)
+    all_y_pred_vectors = nn_model.predict(all_inputs, batch_size=1)
+    all_y_pred=np.zeros(all_y_pred_vectors.shape[0], dtype=int)
+    for elm in enumerate(all_y_pred_vectors):
+        max_idx, *not_used = np.where(elm[1]==np.amax(elm[1]))
+        predicted_vec = np.eye(1,all_y_pred_vectors.shape[1],k=max_idx[0], dtype=int).ravel()
+        all_y_pred[elm[0]] = vector_2_label.get(tuple(predicted_vec))
+    gt_pred_all_map = np.zeros(gt.shape, dtype=int)
+    for elm in enumerate(zip(rr.ravel(), cc.ravel())):
+        gt_pred_all_map[elm[1]] = all_y_pred[elm[0]]
+    
+    if plo:
+        plt.imshow(gt_pred_all_map)
+    return gt_pred_all_map
     
 
     
     
     
-    
+
     
     
     
